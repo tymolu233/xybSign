6ab45c6e63dd0e111dcb25461c5c9dbd953238a5
tymolu <51531250+tymolu233@users.noreply.github.com>
2024-12-15 16:07:57 +0800

    Update config.js

--- config.js
+++ config.js
@@ -35,7 +35,7 @@ const modeCN = {
 config.modeCN = modeCN[config.mode];
 
 const apis = {
-  login: "login/login.action",
+  login: "login/login!wx.action",
   accountInfo: "account/LoadAccountInfo.action",
   projects: "student/progress/ProjectList.action",
   tasks: "student/progress/ProjectProgressInfo.action",
